6f7d8e1deeed21785c02b41ef5157bfb111a516f
BarthesSimpson <d.s.frenk@gmail.com>
2018-10-31 21:53:05 -0700

    double reverse

--- src/index.js
+++ src/index.js
@@ -8,9 +8,24 @@ const example = (
     <h5>List</h5>
     <LinkedList vals={[1, 2, 3, 4, 5]}>{list => list}</LinkedList>
     <br />
+
     <h5>Reversed</h5>
     <LinkedList vals={[1, 2, 3, 4, 5]}>
       {list => <Reverse list={list}>{reversed => reversed}</Reverse>}
+    </LinkedList>
+    <br />
+
+    <h5>Double Reversed!</h5>
+    <LinkedList vals={[1, 2, 3, 4, 5]}>
+      {list => (
+        <Reverse list={list}>
+          {reversed => (
+            <Reverse list={reversed}>
+              {doubleReversed => doubleReversed}
+            </Reverse>
+          )}
+        </Reverse>
+      )}
     </LinkedList>
   </div>
 )
